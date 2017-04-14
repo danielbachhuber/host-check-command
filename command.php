841c2521c57e8ca4c6891aa776881ff74f874e70
Daniel Bachhuber <daniel@handbuilt.co>
2017-04-14 06:56:04 -0700

    Refactor command to use our own load process

--- command.php
+++ command.php
@@ -1,5 +1,7 @@
 <?php
 
+use WP_CLI\Utils;
+
 /**
  * Check that the WordPress install is still hosted at its internal domain.
  */
@@ -20,17 +22,133 @@ class Host_Check_Command {
 	 * @when before_wp_load
 	 */
 	public function __invoke() {
-		$start = microtime( true );
-		$path = WP_CLI::get_config( 'path' );
-		WP_CLI::log( 'Loading: ' . $path );
-		// Disable WP Cron so the update check doesn't run
-		define( 'ALTERNATE_WP_CRON', true );
-		WP_CLI::get_runner()->load_wordpress();
-		$time = microtime( true ) - $start;
-		$load_time = round( $time, 3 ) . 's';
-		WP_CLI::log( 'WordPress load time: ' . $load_time );
+		global $wpdb;
 
-		// Check to see when the next version check would've been
+		$path = realpath( WP_CLI::get_config( 'path' ) );
+		WP_CLI::log( 'Loading: ' . $path );
+
+		// See how far we can get with loading WordPress
+		$status = false;
+		$wp_version = $next_check = '';
+		if ( ! self::wp_exists() ) {
+			$status = 'no-wp-exists';
+		}
+		if ( false === $status ) {
+			$wp_version = self::get_wp_version();
+			WP_CLI::log( 'WordPress version: ' . $wp_version );
+			$wp_config_path = Utils\locate_wp_config();
+			if ( ! $wp_config_path ) {
+				$status = 'no-wp-config';
+			}
+		}
+
+		if ( false === $status ) {
+			self::load_wordpress_lite();
+			if ( ! empty( $wpdb->error ) ) {
+				$status = 'error-db-connect';
+			} else {
+				// Remaining bootstrap steps
+				wp_set_wpdb_vars();
+				wp_start_object_cache();
+				wp_plugin_directory_constants();
+			}
+		}
+
+		if ( false === $status ) {
+			// Check to see when the next version check would've been
+			$next_check = self::get_next_check();
+			$status = self::get_host_status();
+		}
+		WP_CLI::log( "Summary: {$path}, {$wp_version}, {$next_check}, {$status}" );
+	}
+
+	private static function wp_exists() {
+		return is_readable( ABSPATH . 'wp-includes/version.php' );
+	}
+
+	private static function get_wp_version() {
+		global $wp_version;
+		include ABSPATH . 'wp-includes/version.php';
+		return $wp_version;
+	}
+
+	private static function get_wp_config_code() {
+		$wp_config_path = Utils\locate_wp_config();
+		$wp_config_code = explode( "\n", file_get_contents( $wp_config_path ) );
+		$found_wp_settings = false;
+		$lines_to_run = array();
+		foreach ( $wp_config_code as $line ) {
+			if ( preg_match( '/^\s*require.+wp-settings\.php/', $line ) ) {
+				$found_wp_settings = true;
+				continue;
+			}
+			$lines_to_run[] = $line;
+		}
+		if ( !$found_wp_settings ) {
+			WP_CLI::error( 'Strange wp-config.php file: wp-settings.php is not loaded directly.' );
+		}
+		$source = implode( "\n", $lines_to_run );
+		$source = Utils\replace_path_consts( $source, $wp_config_path );
+		return preg_replace( '|^\s*\<\?php\s*|', '', $source );
+	}
+
+	private static function load_wordpress_lite() {
+		// Globals not explicitly globalized in WordPress
+		global $site_id, $wpdb, $public, $current_site, $current_blog, $path, $shortcode_tags;
+
+		// Load wp-config.php code, in the global scope
+		$wp_cli_original_defined_vars = get_defined_vars();
+		eval( self::get_wp_config_code() );
+		foreach( get_defined_vars() as $key => $var ) {
+			if ( array_key_exists( $key, $wp_cli_original_defined_vars ) || 'wp_cli_original_defined_vars' === $key ) {
+				continue;
+			}
+			global $$key;
+			$$key = $var;
+		}
+
+		self::maybe_update_url_from_domain_constant();
+
+		// Behold, wp-settings.php
+		define( 'WPINC', 'wp-includes' );
+
+		// Include files required for initialization.
+		require( ABSPATH . WPINC . '/load.php' );
+		require( ABSPATH . WPINC . '/default-constants.php' );
+		require( ABSPATH . WPINC . '/plugin.php' );
+
+		global $wp_version, $wp_db_version, $tinymce_version, $required_php_version, $required_mysql_version, $wp_local_package;
+		require( ABSPATH . WPINC . '/version.php' );
+
+		global $blog_id;
+		wp_initial_constants();
+		wp_check_php_mysql_versions();
+		@ini_set( 'magic_quotes_runtime', 0 );
+		@ini_set( 'magic_quotes_sybase',  0 );
+		date_default_timezone_set( 'UTC' );
+		wp_unregister_GLOBALS();
+		wp_fix_server_vars();
+		wp_set_lang_dir();
+		require( ABSPATH . WPINC . '/compat.php' );
+		require( ABSPATH . WPINC . '/functions.php' );
+		require( ABSPATH . WPINC . '/class-wp.php' );
+		require( ABSPATH . WPINC . '/class-wp-error.php' );
+		require( ABSPATH . WPINC . '/pomo/mo.php' );
+		require( ABSPATH . WPINC . '/formatting.php' );
+		require_wp_db();
+	}
+
+	private static function maybe_update_url_from_domain_constant() {
+		if ( defined( 'DOMAIN_CURRENT_SITE' ) ) {
+			$url = DOMAIN_CURRENT_SITE;
+			if ( defined( 'PATH_CURRENT_SITE' ) ) {
+				$url .= PATH_CURRENT_SITE;
+			}
+			\WP_CLI::set_url( $url );
+		}
+	}
+
+	private static function get_next_check() {
 		$cron = get_option( 'cron', array() );
 		$next_check = '';
 		if ( is_array( $cron ) ) {
@@ -45,8 +163,10 @@ class Host_Check_Command {
 				}
 			}
 		}
-		WP_CLI::log( 'WordPress version: ' . $GLOBALS['wp_version'] );
+		return $next_check;
+	}
 
+	private static function get_host_status() {
 		// Generate a test file to fetch and check
 		$uuid = md5( mt_rand( 0, 100000 ) );
 		$upload_dir = wp_upload_dir();
@@ -56,28 +176,21 @@ class Host_Check_Command {
 		if ( ! $ret ) {
 			WP_CLI::error( "Couldn't write test file to path: {$test_file_path}" );
 		}
-
 		// Fetch and check the test file
-		$response = wp_remote_get( $upload_dir['baseurl'] . '/' . $test_file );
-		if ( is_wp_error( $response ) ) {
-			WP_CLI::error( $response );
-		}
-		$response_code = wp_remote_retrieve_response_code( $response );
-		$response_body = wp_remote_retrieve_body( $response );
-		if ( $uuid === $response_body ) {
+		$response = Utils\http_request( 'GET', $upload_dir['baseurl'] . '/' . $test_file );
+		if ( $uuid === $response->body ) {
 			$status = 'yes';
-			WP_CLI::log( "Yes: WordPress install is hosted here (HTTP code {$response_code})" );
+			WP_CLI::log( "Yes: WordPress install is hosted here (HTTP code {$response->status_code})" );
 		} else {
 			$status = 'no';
-			WP_CLI::log( "No: WordPress install isn't hosted here (HTTP code {$response_code})" );
+			WP_CLI::log( "No: WordPress install isn't hosted here (HTTP code {$response->status_code})" );
 		}
-
 		// Don't need the test file anymore
 		$ret = unlink( $test_file_path );
 		if ( ! $ret ) {
 			WP_CLI::error( "Couldn't delete test file: {$test_file_path}" );
 		}
-		WP_CLI::log( "Summary: {$path}, {$load_time}, {$GLOBALS['wp_version']}, {$next_check}, {$status}" );
+		return $status;
 	}
 
 }
