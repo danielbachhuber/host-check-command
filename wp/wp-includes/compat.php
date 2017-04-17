0e57e7b8490faf0e13ddfda3dc35c2b7656c0784
Daniel Bachhuber <daniel@handbuilt.co>
2017-04-17 13:27:34 -0700

    Add a missing compat file

--- wp/wp-includes/compat.php
+++ wp/wp-includes/compat.php
@@ -432,7 +432,7 @@ if ( ! interface_exists( 'JsonSerializable' ) ) {
 
 // random_int was introduced in PHP 7.0
 if ( ! function_exists( 'random_int' ) ) {
-	require ABSPATH . WPINC . '/random_compat/random.php';
+	require COMMAND_ABSPATH . WPINC . '/random_compat/random.php';
 }
 
 if ( ! function_exists( 'array_replace_recursive' ) ) :
